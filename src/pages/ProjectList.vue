4b03a56ae55b72ceb4e1f79e19ca9db42ed1d0b3
NeilMagon <neil.magon97@gmail.com>
2024-06-13 16:45:13 +0200

    feat: add next and previous currentPage

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -9,19 +9,29 @@
         },
         data() {
             return {
-                projects: []
+                projects: [],
+                currentPage: 1,
+                nextPageUrl: null,
+                prevPageUrl: null
             }
         },
         methods: {
-            getProject(){
-                axios.get('http://127.0.0.1:8000/api/projects')
+            getProject(pageShow){
+                axios.get('http://127.0.0.1:8000/api/projects',
+                   { params: {
+                        page: pageShow
+                    }
+                })
                 .then((response) => {
-                    this.projects = response.data.results;
+                    this.projects = response.data.results.data;
+                    this.currentPage = response.data.results.current_page;
+                    this.nextPageUrl = response.data.results.next_page_url;
+                    this.prevPageUrl = response.data.results.prev_page_url;
                 });
             }
         },
         mounted() {
-            this.getProject();
+            this.getProject(this.currentPage);
         }
     }
 </script>
@@ -30,6 +40,16 @@
     <h1>Project List</h1>
     <div class="row row-cols-3">
         <ProjectCard v-for="project in projects" :projectInfo="project"></ProjectCard>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li v-if="prevPageUrl" class="page-item">
+                    <a class="page-link" @click="getProject(currentPage - 1)">Previous</a>
+                </li>
+                <li v-if="nextPageUrl" class="page-item">
+                    <a class="page-link" @click="getProject(currentPage + 1)">Next</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
